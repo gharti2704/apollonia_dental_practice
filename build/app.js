4a289b6ed41612c8c391e7dbe38d41d1d85f26d0
Raju Gharti <gharti.rg@gmail.com>
2024-03-01 06:21:25 -0500

    initial setup

---
+++ build/app.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+// import router from './route/auth.js';
+// import { db } from './data/index.js';
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, morgan_1.default)('dev'));
+// app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+    app.use((0, cors_1.default)({
+        origin: process.env.DEVELOPMENT_CLIENT_URL,
+    }));
+}
+else if (process.env.NODE_ENV === 'production') {
+    app.use((0, cors_1.default)({
+        origin: process.env.PRODUCTION_CLIENT_URL,
+    }));
+}
+// app.use('/api/auth', router);
+const port = process.env.PORT || 8000;
+app.listen(port, async () => {
+    console.log(`Server is running on http://localhost:${port} - ${process.env.NODE_ENV} mode`);
+    // await db.initDb();
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express, { Express } from 'express';
+// import router from './route/auth.js';
+// import { db } from './data/index.js';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+// app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(
+    cors({
+      origin: process.env.DEVELOPMENT_CLIENT_URL,
+    })
+  );
+} else if (process.env.NODE_ENV === 'production') {
+  app.use(
+    cors({
+      origin: process.env.PRODUCTION_CLIENT_URL,
+    })
+  );
+}
+
+// app.use('/api/auth', router);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, async () => {
+  console.log(
+    `Server is running on http://localhost:${port} - ${process.env.NODE_ENV} mode`
+  );
+  // await db.initDb();
+});
